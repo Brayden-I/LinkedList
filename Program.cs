409a68f17c12078e6e273aee0404982e2faaf1d9
Brayden-I <bi1012440@otc.edu>
2026-03-05 18:30:21 -0600

    included all the methods from LLNoder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using LLNoder;
+﻿// BRAYDEN IMADA
+using LLNoder;
 
 namespace LinkedListApp;
 
@@ -6,10 +7,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        LinkedList linkedList = new LinkedList();
+        LLinkedList<int> linkedList = new LLinkedList<int>();
 
-        linkedList.addNode(15);
-        linkedList.addNode(25);
-        linkedList.addNode(65);
+        linkedList.AddNode(15);
+        linkedList.AddNode(25);
+        linkedList.AddNode(65);
+
+        linkedList.Print();
+
+        Console.WriteLine($"{linkedList.Search(25)}");
+
+        linkedList.DeleteNode(25);
+        linkedList.AddNode(65);
+
+        linkedList.Print();
+
+        Console.WriteLine($"{linkedList.Search(65)}");
+
+        linkedList.DeleteNode(65);
+        linkedList.DeleteNode(65);
+        linkedList.DeleteNode(65);
+
+        linkedList.Print();
     }
 }
